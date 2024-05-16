9c90893ac1c5e026f8b1c1e61c79713c00aac4fd
binyongbin <kyb2475@gmail.com>
2024-05-16 22:20:37 +0900

    feat: 비밀번호 인풋 유효성 검사 기능 추가

--- js/validateInput.js
+++ js/validateInput.js
@@ -1,10 +1,18 @@
 const emailInput = document.querySelector(".login-input-field");
-const emailErrorMessage = document.querySelector(".error-message");
+const emailErrorMessage = document.querySelector(".login-error-message");
+
+const passwordInput = document.querySelector(".password-field");
+const passwordErrorMessage = document.querySelector(".password-error-message");
 
 const isEmailValid = (email) => {
-  const pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
-  const isEmail = pattern.test(email);
+  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+  const isEmail = emailPattern.test(email);
   return isEmail;
+};
+
+const isPasswordValid = (password) => {
+  const isPassword = password.length >= 8;
+  return isPassword;
 };
 
 const validateEmailInput = (email) => {
@@ -27,6 +35,30 @@ const validateEmailInput = (email) => {
   emailErrorMessage.textContent = "";
 };
 
+const validatePasswordInput = (password) => {
+  if (password === "") {
+    passwordInput.className += " password-field-error";
+    passwordErrorMessage.classList += " error-message-on";
+    passwordErrorMessage.textContent = "비밀번호를 입력해주세요.";
+    return;
+  }
+
+  if (!isPasswordValid(password)) {
+    passwordInput.className += " password-field-error";
+    passwordErrorMessage.classList += " error-message-on";
+    passwordErrorMessage.textContent = "비밀번호를 8자 이상 입력해주세요.";
+    return;
+  }
+
+  passwordInput.classList.remove("password-field-error");
+  passwordErrorMessage.classList.remove("error-message-on");
+  passwordErrorMessage.textContent = "";
+};
+
 emailInput.addEventListener("focusout", () => {
   validateEmailInput(emailInput.value);
+});
+
+passwordInput.addEventListener("focusout", () => {
+  validatePasswordInput(passwordInput.value);
 });
