a0765ec696d77e1413423c38372aeb852f810012
binyongbin <kyb2475@gmail.com>
2024-05-16 21:47:54 +0900

    feat: 이메일 인풋 유효성 검사 기능 추가

--- js/validateInput.js
+++ js/validateInput.js
@@ -1,8 +1,11 @@
-let isEmailValid = false;
-let isPasswordValid = false;
-
 const emailInput = document.querySelector(".login-input-field");
 const emailErrorMessage = document.querySelector(".error-message");
+
+const isEmailValid = (email) => {
+  const pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+  const isEmail = pattern.test(email);
+  return isEmail;
+};
 
 const validateEmailInput = (email) => {
   if (email === "") {
@@ -11,6 +14,17 @@ const validateEmailInput = (email) => {
     emailErrorMessage.textContent = "이메일을 입력해주세요.";
     return;
   }
+
+  if (!isEmailValid(email)) {
+    emailInput.className += " login-input-error";
+    emailErrorMessage.classList += " error-message-on";
+    emailErrorMessage.textContent = "올바른 이메일을 입력해주세요.";
+    return;
+  }
+
+  emailInput.classList.remove("login-input-error");
+  emailErrorMessage.classList.remove("error-message-on");
+  emailErrorMessage.textContent = "";
 };
 
 emailInput.addEventListener("focusout", () => {
