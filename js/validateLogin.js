60124b4861c8db68c0dc04c13043dad0f00a9924
binyongbin <kyb2475@gmail.com>
2024-05-17 22:44:17 +0900

    feat: 로그인 폼 유효성 검사 결과에 따른 버튼 상태 기능 추가

--- js/validateLogin.js
+++ js/validateLogin.js
@@ -4,6 +4,8 @@ const emailErrorMessage = document.querySelector(".email-error-message");
 const passwordInput = document.querySelector(".auth-password-field");
 const passwordErrorMessage = document.querySelector(".password-error-message");
 
+const loginButton = document.querySelector(".auth-button");
+
 const isEmailValid = (email) => {
   const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
   const isEmail = emailPattern.test(email);
@@ -20,7 +22,7 @@ const validateEmailInput = (email) => {
     emailInput.classList.add("auth-input-error");
     emailErrorMessage.classList.add("error-message-on");
     emailErrorMessage.textContent = "이메일을 입력해주세요.";
-    return;
+    return false;
   }
 
   if (!isEmailValid(email)) {
@@ -30,12 +32,13 @@ const validateEmailInput = (email) => {
     emailInput.classList.add("auth-input-error");
     emailErrorMessage.classList.add("error-message-on");
     emailErrorMessage.textContent = "잘못된 이메일 형식입니다.";
-    return;
+    return false;
   }
 
   emailInput.classList.remove("auth-input-error");
   emailErrorMessage.classList.remove("error-message-on");
   emailErrorMessage.textContent = "";
+  return true;
 };
 
 const validatePasswordInput = (password) => {
@@ -43,7 +46,7 @@ const validatePasswordInput = (password) => {
     passwordInput.classList.add("password-field-error");
     passwordErrorMessage.classList.add("error-message-on");
     passwordErrorMessage.textContent = "비밀번호를 입력해주세요.";
-    return;
+    return false;
   }
 
   if (!isPasswordValid(password)) {
@@ -53,12 +56,13 @@ const validatePasswordInput = (password) => {
     passwordInput.classList.add("password-field-error");
     passwordErrorMessage.classList.add("error-message-on");
     passwordErrorMessage.textContent = "비밀번호를 8자 이상 입력해주세요.";
-    return;
+    return false;
   }
 
   passwordInput.classList.remove("password-field-error");
   passwordErrorMessage.classList.remove("error-message-on");
   passwordErrorMessage.textContent = "";
+  return true;
 };
 
 emailInput.addEventListener("focusout", () => {
@@ -67,4 +71,47 @@ emailInput.addEventListener("focusout", () => {
 
 passwordInput.addEventListener("focusout", () => {
   validatePasswordInput(passwordInput.value);
+});
+
+emailInput.addEventListener("input", () => {
+  if (
+    emailInput.value !== "" &&
+    passwordInput.value !== "" &&
+    validateEmailInput(emailInput.value) === true &&
+    validatePasswordInput(passwordInput.value) === true
+  ) {
+    loginButton.removeAttribute("disabled", "");
+    loginButton.classList.remove("button-disabled");
+  } else {
+    loginButton.setAttribute("disabled", "");
+    loginButton.classList.add("button-disabled");
+  }
+});
+
+passwordInput.addEventListener("input", () => {
+  if (
+    emailInput.value !== "" &&
+    passwordInput.value !== "" &&
+    validateEmailInput(emailInput.value) === true &&
+    validatePasswordInput(passwordInput.value) === true
+  ) {
+    loginButton.removeAttribute("disabled", "");
+    loginButton.classList.remove("button-disabled");
+  } else {
+    loginButton.setAttribute("disabled", "");
+    loginButton.classList.add("button-disabled");
+  }
+});
+
+loginButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (
+    emailInput.value !== "" &&
+    passwordInput.value !== "" &&
+    validateEmailInput(emailInput.value) === true &&
+    validatePasswordInput(passwordInput.value) === true
+  ) {
+    window.location.href = "/items";
+  }
 });
