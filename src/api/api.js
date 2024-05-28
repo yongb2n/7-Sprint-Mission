70ab8aff7a9d49f60fa1a10a962ec41469e10b71
binyongbin <kyb2475@gmail.com>
2024-05-28 23:57:08 +0900

    feat: Add api.js

---
+++ src/api/api.js
@@ -0,0 +1,14 @@
+const PRODUCTS_URL = "panda-market-api.vercel.app/products";
+
+export async function getProducts({
+  page = 1,
+  pageSize = 10,
+  orderBy = "recent",
+  keyword = "",
+}) {
+  const query = `page=${page}&pageSize=${pageSize}&orderBy=${orderBy}&keyword=${keyword}`;
+
+  const response = await fetch(`${PRODUCTS_URL}?${query}`);
+  const body = await response.json();
+  return body;
+}
