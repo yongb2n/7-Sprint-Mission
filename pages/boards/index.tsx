6afbfe88df678ece5e271d0a453383966ba6d674
binyongbin <kyb2475@gmail.com>
2024-07-13 19:30:34 +0900

    feat: Boards 페이지 반응형 적용

--- pages/boards/index.tsx
+++ pages/boards/index.tsx
@@ -1,12 +1,13 @@
 import All from "./components/All";
 import Best from "./components/Best";
+import styles from "./styles.module.scss";
 
 function Boards() {
   return (
-    <>
+    <div className={styles["container"]}>
       <Best />
       <All />
-    </>
+    </div>
   );
 }
 
