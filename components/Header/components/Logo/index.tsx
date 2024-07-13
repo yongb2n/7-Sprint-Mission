9f850426c6a6e4c62d18f9e8bb471ba75daafa76
binyongbin <kyb2475@gmail.com>
2024-07-13 20:19:38 +0900

    refactor: Header 컴포넌트 리팩토링

--- components/Header/components/Logo/index.tsx
+++ components/Header/components/Logo/index.tsx
@@ -1,37 +1,21 @@
-import { useState, useEffect } from "react";
 import styles from "./styles.module.scss";
 import Link from "next/link";
 import Image from "next/image";
 import logo from "@/assets/icons/logo/logo.svg";
 import mobileLogo from "@/assets/icons/logo/mobile-logo.svg";
-import debounce from "lodash-es/debounce";
 
 function Logo() {
-  const [currentLogo, setCurrentLogo] = useState(logo);
-
-  useEffect(() => {
-    const handleResize = debounce(() => {
-      if (window.innerWidth <= 767) {
-        setCurrentLogo(mobileLogo);
-      } else {
-        setCurrentLogo(logo);
-      }
-    }, 300);
-
-    handleResize();
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-      handleResize.cancel();
-    };
-  }, []);
-
   return (
     <h1>
       <Link href="/">
-        <Image className={styles["logo"]} src={currentLogo} alt="판다마켓" />
+        <Image className={styles["logo"]} src={logo} alt="판다마켓" />
+      </Link>
+      <Link href="/">
+        <Image
+          className={styles["mobile-logo"]}
+          src={mobileLogo}
+          alt="판다마켓"
+        />
       </Link>
     </h1>
   );
