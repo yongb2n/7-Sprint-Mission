85bba4d42744e3f44965d813a552f0983977276c
binyongbin <kyb2475@gmail.com>
2024-07-13 17:35:39 +0900

    chore: 코드 포맷팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import Header from '@/components/Header';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Header />
+      <Header /> 
       <Component {...pageProps} />
     </>
   );
