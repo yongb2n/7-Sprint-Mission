7d37275027a516d1919080ba8b10abeb7d3677ae
binyongbin <kyb2475@gmail.com>
2024-07-13 19:31:28 +0900

    feat: BestPost 컴포넌트 반응형에 따라 보이는 컴포넌트 개수 설정

--- pages/boards/components/Best/index.tsx
+++ pages/boards/components/Best/index.tsx
@@ -1,18 +1,19 @@
-// Best.tsx
 import { useEffect, useState } from "react";
 import BestPost from "./BestPost";
 import styles from "./styles.module.scss";
 import { getArticles, Article } from "@/pages/api/articles";
+import debounce from "lodash-es/debounce";
 
 function Best() {
   const [articles, setArticles] = useState<Article[]>([]);
+  const [pageSize, setPageSize] = useState(3);
 
   useEffect(() => {
     async function fetchArticles() {
       try {
         const params = {
           page: 1,
-          pageSize: 3,
+          pageSize: pageSize,
           orderBy: "like",
           keyword: "",
         };
@@ -24,13 +25,33 @@ function Best() {
     }
 
     fetchArticles();
+  }, [pageSize]);
+
+  useEffect(() => {
+    const handleResize = debounce(() => {
+      if (window.innerWidth >= 1200) {
+        setPageSize(3);
+      } else if (window.innerWidth >= 768 && window.innerWidth <= 1199) {
+        setPageSize(2);
+      } else if (window.innerWidth <= 767) {
+        setPageSize(1);
+      }
+    }, 100);
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
 
   return (
     <div className={styles["container"]}>
       <label className={styles["title"]}>베스트 게시글</label>
       <div className={styles["best-post-wrapper"]}>
-        {articles.map((article) => (
+        {articles.slice(0, pageSize).map((article) => (
           <BestPost key={article.id} article={article} />
         ))}
       </div>
