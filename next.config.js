a7f0a2ef38837b08ea18813e4a72f12744e1033c
binyongbin <kyb2475@gmail.com>
2024-07-20 15:04:36 +0900

    feat: next.config.js에 domain 추가

--- next.config.js
+++ next.config.js
@@ -2,8 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['sprint-fe-project.s3.ap-northeast-2.amazonaws.com'],
+    domains: [
+      "sprint-fe-project.s3.ap-northeast-2.amazonaws.com",
+      "example.com",
+    ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
