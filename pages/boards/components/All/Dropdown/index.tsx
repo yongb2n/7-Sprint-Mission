c88b70b7a13f6375f9fe1521e3c9fffcf4bbbf52
binyongbin <kyb2475@gmail.com>
2024-07-20 15:28:13 +0900

    refactor: 드롭다운 반응형 CSS로 리팩토링

--- pages/boards/components/All/Dropdown/index.tsx
+++ pages/boards/components/All/Dropdown/index.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import styles from "./styles.module.scss";
 import Image from "next/image";
 import arrowIcon from "@/assets/icons/ic_arrow_down.svg";
 import sortIcon from "@/assets/icons/ic_sort.svg";
-import debounce from "lodash-es/debounce";
+
 interface DropdownProps {
   options: string[];
   defaultOption: string;
@@ -12,21 +12,7 @@ interface DropdownProps {
 
 const Dropdown = ({ options, defaultOption, onChange }: DropdownProps) => {
   const [isOpen, setIsOpen] = useState(false);
-  const [isMobile, setIsMobile] = useState(false);
   const [selectedOption, setSelectedOption] = useState(defaultOption);
-
-  useEffect(() => {
-    const handleResize = debounce(() => {
-      setIsMobile(window.innerWidth <= 767);
-    }, 100);
-
-    handleResize();
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
 
   const toggleDropdown = () => setIsOpen(!isOpen);
 
@@ -35,31 +21,30 @@ const Dropdown = ({ options, defaultOption, onChange }: DropdownProps) => {
     setIsOpen(false);
     onChange(option);
   };
+
   return (
     <div className={styles["container"]}>
-      {!isMobile ? (
-        <div className={styles["dropdown"]}>
-          <div className={styles["dropdown-header"]} onClick={toggleDropdown}>
-            <span className={styles["dropdown-option"]}>{selectedOption}</span>
-            <Image
-              src={arrowIcon}
-              alt="화살표"
-              className={styles["arrow-icon"]}
-              width={24}
-              height={24}
-            />
-          </div>
+      <div className={styles["dropdown"]}>
+        <div className={styles["dropdown-header"]} onClick={toggleDropdown}>
+          <span className={styles["dropdown-option"]}>{selectedOption}</span>
+          <Image
+            src={arrowIcon}
+            alt="화살표"
+            className={styles["arrow-icon"]}
+            width={24}
+            height={24}
+          />
         </div>
-      ) : (
-        <div className={styles["mobile-dropdown"]}>
-          <div
-            className={styles["mobile-dropdown-header"]}
-            onClick={toggleDropdown}
-          >
-            <Image src={sortIcon} alt="정렬" width={24} height={24} />
-          </div>
+      </div>
+
+      <div className={styles["mobile-dropdown"]}>
+        <div
+          className={styles["mobile-dropdown-header"]}
+          onClick={toggleDropdown}
+        >
+          <Image src={sortIcon} alt="정렬" width={24} height={24} />
         </div>
-      )}
+      </div>
 
       {isOpen && (
         <ul className={styles["dropdown-list"]}>
