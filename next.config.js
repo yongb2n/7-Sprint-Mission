3f5ee40145a87a62c3930670590af1e68dd86237
binyongbin <kyb2475@gmail.com>
2024-07-21 20:26:02 +0900

    refactor: domains > images.remotePatterns 설정 변경

--- next.config.js
+++ next.config.js
@@ -2,9 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [
-      "sprint-fe-project.s3.ap-northeast-2.amazonaws.com",
-      "example.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "sprint-fe-project.s3.ap-northeast-2.amazonaws.com",
+        port: "",
+        pathname: "/Sprint_Mission/**",
+      },
     ],
   },
 };
