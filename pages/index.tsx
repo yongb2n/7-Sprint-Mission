f893130d8b84583d3d76e0cc64959574385699be
binyongbin <kyb2475@gmail.com>
2024-07-23 22:28:26 +0900

    refactor: 메인페이지 path /landing 으로 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,12 @@
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
-  return <></>;
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/landing");
+  }, [router]);
+
+  return null;
 }
